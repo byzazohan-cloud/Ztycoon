2ce2d021ec43eaaae42184acab7c95f01c48ec3c
byzazohan-cloud <byzazohan@gmail.com>
2026-09-14 20:02:53 +0300

    Add files via upload

---
+++ README_V14.txt
@@ -0,0 +1,9 @@
+ZAZO TYCOON V14 — CONCEPT UI
+- Ana ekran referans görseldeki koyu lacivert + altın mobil tasarıma yaklaştırıldı.
+- Şirketlerim listesi yatay ve kompakt hale getirildi.
+- Dashboard şirket mini kartları eklendi.
+- Şirket kurma ekranına 4 adımlı wizard görünümü eklendi.
+- Logo Studio daha büyük önizleme ve kompakt sembol gridine dönüştürüldü.
+- 9 ofis seviyesi ana ekranda 3x3 galeri olarak gösterilir.
+- Banka ekranı Z BANK lobi + sol menü yapısına geçirildi.
+- V13 canlı satış, depo, şirket yönetimi, Logo Studio ve ofis editörü korunur.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zazo-tycoon-v13-complete';
+const CACHE='zazo-tycoon-v14-concept-ui';
 const ASSETS=["./", "./index.html", "./office.jpg", "./bank_v13.jpg", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./office_v13_1.jpg", "./office_v13_1_4k.jpg", "./office_v13_2.jpg", "./office_v13_2_4k.jpg", "./office_v13_3.jpg", "./office_v13_3_4k.jpg", "./office_v13_4.jpg", "./office_v13_4_4k.jpg", "./office_v13_5.jpg", "./office_v13_5_4k.jpg", "./office_v13_6.jpg", "./office_v13_6_4k.jpg", "./office_v13_7.jpg", "./office_v13_7_4k.jpg", "./office_v13_8.jpg", "./office_v13_8_4k.jpg", "./office_v13_9.jpg", "./office_v13_9_4k.jpg"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
