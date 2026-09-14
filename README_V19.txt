79d9b0ecd74debe9db76d4eccaa157414de6c0fd
byzazohan-cloud <byzazohan@gmail.com>
2026-09-14 21:41:04 +0300

    Add files via upload

---
+++ README_V19.txt
@@ -0,0 +1,9 @@
+ZAZO TYCOON V19 REFERENCE MATCH
+
+- Yazılar büyütüldü, butonlar küçültüldü.
+- Görsel alanları 16:9 sabitlendi.
+- Banka ve ofis bağımsız yüksek kalite görsellerle değiştirildi.
+- Şirket Kur gerçek 4 adımlı wizard: Sektör > Logo > Bilgiler > Tamamla.
+- Logo sekmesi tıklanabilir; sembol, renk, monogram seçilir.
+- Seçilen logo şirket kartında kullanılır.
+- Banka Kartlar sekmesi aktif; kart limiti, borç, borç ödeme ve ödemede kartı kullanma çalışır.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zazo-tycoon-v18-ultra-mobile';
+const CACHE='zazo-tycoon-v19-reference-match';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./bank_mobile.jpg','./bank_8k.jpg','./office_mobile.jpg','./office_8k.jpg','./shop_mobile.jpg','./shop_8k.jpg','./warehouse_mobile.jpg','./warehouse_8k.jpg','./factory_mobile.jpg','./factory_8k.jpg','./city_mobile.jpg','./city_8k.jpg','./market_mobile.jpg','./market_8k.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
