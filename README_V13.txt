360c8010745d257c498b3a70156aed8469671bf6
byzazohan-cloud <byzazohan@gmail.com>
2026-09-14 19:54:38 +0300

    Add files via upload

---
+++ README_V13.txt
@@ -0,0 +1,25 @@
+ZAZO TYCOON V13 COMPLETE
+
+BU SÜRÜMDE GERÇEKTEN DEĞİŞENLER:
+- Oyun adı: ZAZO TYCOON
+- Yeni 9 seviyeli düz açılı ofis görselleri oyuna bağlandı:
+  1 Standart
+  2 Gelişen
+  3 Modern
+  4 Prestijli
+  5 Yönetici
+  6 Üst Düzey
+  7 CEO
+  8 Holding Merkezi
+  9 Penthouse
+- Şirket adı/logo için duvar alanı uygulamanın dinamik katmanıdır.
+- Kullanıcı Logo Studio ile özel şirket logosu oluşturabilir.
+- Kompakt şirket kartları ve küçük detay düğmeleri korunur.
+- Yeni tek sahneli Z BANK iç mekânı bağlandı.
+- V10 canlı satış + depo sistemi korunur.
+- V9 mobil duvar/ofis editörü korunur.
+- PWA cache sistemi düzeltildi: HTML için network-first ve zorunlu service worker update.
+
+GITHUB:
+ZIP içindeki TÜM dosyaları repo köküne yükleyin ve eski dosyaların üzerine yazın.
+Sayfayı ilk açıştan sonra bir kez yenileyin.
--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE='zazo-tycoon-v12';const ASSETS=['./','./index.html','./office.jpg','./office_level_6_8k.jpg','./office_level_5_8k.jpg','./office_level_4_8k.jpg','./office_level_3_8k.jpg','./office_level_2_8k.jpg','./office_level_1_8k.jpg','./bank_lobby.jpg','./office_level_1.jpg','./office_level_2.jpg','./office_level_3.jpg','./office_level_4.jpg','./office_level_5.jpg','./office_level_6.jpg','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='zazo-tycoon-v13-complete';
+const ASSETS=["./", "./index.html", "./office.jpg", "./bank_v13.jpg", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./office_v13_1.jpg", "./office_v13_1_4k.jpg", "./office_v13_2.jpg", "./office_v13_2_4k.jpg", "./office_v13_3.jpg", "./office_v13_3_4k.jpg", "./office_v13_4.jpg", "./office_v13_4_4k.jpg", "./office_v13_5.jpg", "./office_v13_5_4k.jpg", "./office_v13_6.jpg", "./office_v13_6_4k.jpg", "./office_v13_7.jpg", "./office_v13_7_4k.jpg", "./office_v13_8.jpg", "./office_v13_8_4k.jpg", "./office_v13_9.jpg", "./office_v13_9_4k.jpg"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ const r=e.request;
+ if(r.mode==='navigate'||(r.headers.get('accept')||'').includes('text/html')){
+   e.respondWith(fetch(r).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res}).catch(()=>caches.match(r).then(x=>x||caches.match('./index.html'))));
+   return;
+ }
+ e.respondWith(caches.match(r).then(x=>x||fetch(r).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res})));
+});
