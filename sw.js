2681496d15a10e48dcc515f7c20a09519879b402
byzazohan-cloud <byzazohan@gmail.com>
2026-09-19 20:20:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zazo-tycoon-2.5.1';
+const CACHE='zazo-tycoon-2.6.1';
 const ASSETS=['./','./index.html','./assets/zazo_city_world.webp','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
