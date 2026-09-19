45bf872464e06383f99c0fe8f741d82cb3c7fa7b
byzazohan-cloud <byzazohan@gmail.com>
2026-09-20 00:02:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
-const CACHE='zazo-tycoon-3.5.2';
+const CACHE='zazo-tycoon-3.9.3';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(hit=>hit||fetch(e.request).catch(()=>{
-      if(e.request.mode==='navigate') return caches.match('./index.html');
-      return new Response('',{status:503,statusText:'Offline'});
-    }))
-  );
-});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
